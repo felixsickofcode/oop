158c589f5667f6763da116ecce68dc5a5d4b6544
felixsickofcode <nguyenkhanhphongdv3@gmail.com>
2025-09-21 10:19:00 +0700

    HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Họ Tên: Nguyễn Khánh Phong\tMSSV: 24021594\tLớp: CS5\tUsername GitHub: felixsickofcode\tGmail: 24021594@vnu.edu.vn");
+
+        for ( int  i = 9; i >= 1; i-- ) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
