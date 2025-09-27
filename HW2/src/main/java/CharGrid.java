089cd3c2447a399a3e68abfe047da99909dfbddf
felixsickofcode <nguyenkhanhphongdv3@gmail.com>
2025-09-28 01:01:27 +0700

    HW2

--- HW2/src/main/java/CharGrid.java
+++ HW2/src/main/java/CharGrid.java
@@ -3,32 +3,106 @@
 // a few operations on the grid.
 
 public class CharGrid {
-	private char[][] grid;
+    private char[][] grid;
 
-	/**
-	 * Constructs a new CharGrid with the given grid.
-	 * Does not make a copy.
-	 * @param grid
-	 */
-	public CharGrid(char[][] grid) {
-		this.grid = grid;
-	}
-	
-	/**
-	 * Returns the area for the given char in the grid. (see handout).
-	 * @param ch char to look for
-	 * @return area for given char
-	 */
-	public int charArea(char ch) {
-		return 0; // YOUR CODE HERE
-	}
-	
-	/**
-	 * Returns the count of '+' figures in the grid (see handout).
-	 * @return number of + in grid
-	 */
-	public int countPlus() {
-		return 0; // YOUR CODE HERE
-	}
-	
+    /**
+     * Constructs a new CharGrid with the given grid.
+     * Does not make a copy.
+     *
+     * @param grid
+     */
+    public CharGrid(char[][] grid) {
+        this.grid = grid;
+    }
+
+    /**
+     * Returns the area for the given char in the grid. (see handout).
+     *
+     * @param ch char to look for
+     * @return area for given char
+     */
+    public int charArea(char ch) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int minRow = rows, maxRow = -1, minCol = cols, maxCol = -1;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == ch) {
+                    if (i < minRow) minRow = i;
+                    if (i > maxRow) maxRow = i;
+                    if (j < minCol) minCol = j;
+                    if (j > maxCol) maxCol = j;
+                }
+            }
+        }
+        if (maxRow == -1) return 0;
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+    }
+
+    /**
+     * Returns the count of '+' figures in the grid (see handout).
+     *
+     * @return number of + in grid
+     */
+    public int countPlus() {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int cnt = 0;
+
+        for (int i = 1; i < rows - 1; i++) {
+            for (int j = 1; j < cols - 1; j++) {
+                char c = grid[i][j];
+                if (c == ' ') continue;
+
+                int up = countDir(i, j, -1, 0, c);
+                int down = countDir(i, j, 1, 0, c);
+                int left = countDir(i, j, 0, -1, c);
+                int right = countDir(i, j, 0, 1, c);
+
+                if (up >= 2 && up == down && up == left && up == right) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    private int countDir(int r, int c, int dr, int dc, char ch) {
+        int len = 0;
+        r += dr;
+        c += dc;
+        while (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] == ch) {
+            len++;
+            r += dr;
+            c += dc;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        char[][] grid1 = {
+                {'a', 'b', 'c', 'd'},
+                {'a', ' ', 'c', 'b'},
+                {'x', 'b', 'c', 'a'}
+        };
+        CharGrid cg1 = new CharGrid(grid1);
+        System.out.println("charArea('a') = " + cg1.charArea('a'));
+        System.out.println("charArea('c') = " + cg1.charArea('c'));
+        System.out.println("charArea('z') = " + cg1.charArea('z'));
+
+        char[][] grid2 = {
+                {' ', ' ', 'p', ' ', ' ', ' ', ' ', 'x', ' '},
+                {' ', ' ', 'p', ' ', ' ', ' ', 'x', ' ', ' '},
+                {'p', 'p', 'p', 'p', 'p', ' ', 'x', 'x', 'x'},
+                {' ', ' ', 'p', ' ', ' ', 'y', ' ', 'x', ' '},
+                {' ', ' ', 'p', ' ', 'y', 'y', 'y', ' ', ' '},
+                {'z', 'z', 'z', 'z', 'z', 'y', 'z', 'z', 'z'},
+                {' ', ' ', 'x', 'x', ' ', 'y', ' ', ' ', ' '}
+        };
+        CharGrid cg2 = new CharGrid(grid2);
+        System.out.println("countPlus = " + cg2.countPlus()); // 2
+    }
 }
+
+
--- HW2/src/main/java/StringCode.java
+++ HW2/src/main/java/StringCode.java
@@ -5,35 +5,72 @@ import java.util.Set;
 
 public class StringCode {
 
-	/**
-	 * Given a string, returns the length of the largest run.
-	 * A a run is a series of adajcent chars that are the same.
-	 * @param str
-	 * @return max run length
-	 */
-	public static int maxRun(String str) {
-		return 0; // YOUR CODE HERE
-	}
+    /**
+     * Given a string, returns the length of the largest run.
+     * A a run is a series of adajcent chars that are the same.
+     *
+     * @param str
+     * @return max run length
+     */
+    public static int maxRun(String str) {
+        int max = 0;
+        int temp = 1;
+        for (int i = 1; i < str.length(); i++) {
+            char c = str.charAt(i);
+            char c1 = str.charAt(i - 1);
+            if (c == c1) {
+                temp++;
+            } else temp = 1;
+            if (temp > max) max = temp;
+        }
+        return max;
+    }
 
-	
-	/**
-	 * Given a string, for each digit in the original string,
-	 * replaces the digit with that many occurrences of the character
-	 * following. So the string "a3tx2z" yields "attttxzzz".
-	 * @param str
-	 * @return blown up string
-	 */
-	public static String blowup(String str) {
-		return null; // YOUR CODE HERE
-	}
-	
-	/**
-	 * Given 2 strings, consider all the substrings within them
-	 * of length len. Returns true if there are any such substrings
-	 * which appear in both strings.
-	 * Compute this in linear time using a HashSet. Len will be 1 or more.
-	 */
-	public static boolean stringIntersect(String a, String b, int len) {
-		return false; // YOUR CODE HERE
-	}
+
+    /**
+     * Given a string, for each digit in the original string,
+     * replaces the digit with that many occurrences of the character
+     * following. So the string "a3tx2z" yields "attttxzzz".
+     *
+     * @param str
+     * @return blown up string
+     */
+    public static String blowup(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isDigit(c)) {
+                int k = c - '0';
+                if (i + 1 < str.length()) {
+                    for (int j = 0; j < k; j++) sb.append(str.charAt(i + 1));
+                }
+            } else sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Given 2 strings, consider all the substrings within them
+     * of length len. Returns true if there are any such substrings
+     * which appear in both strings.
+     * Compute this in linear time using a HashSet. Len will be 1 or more.
+     */
+    public static boolean stringIntersect(String a, String b, int len) {
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i + len <= a.length(); i++) {
+            set.add(a.substring(i, i + len));
+        }
+        for (int j = 0; j + len <= b.length(); j++) {
+            if (set.contains(b.substring(j, j + len))) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        StringCode obj = new StringCode();
+        String s = "ab2c3s";
+        String s1 = obj.blowup(s);
+        System.out.println(obj.blowup(s));
+        System.out.println(obj.maxRun(s1));
+    }
 }
